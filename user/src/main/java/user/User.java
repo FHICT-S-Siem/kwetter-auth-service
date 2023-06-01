7fbdc74380009f659d434e48f1f2a85e4816889e
SiemLuc <siem228@hotmail.com>
2023-06-01 13:17:31 +0200

    Fixed sum stuff

--- user/src/main/java/user/User.java
+++ user/src/main/java/user/User.java
@@ -1,10 +1,17 @@
 package user;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class User {
 
     private Long id;
-
     private String name;
+    private String email;
+    private String picture;
+    private String nickname;
+    private String sub;
+//    private Date updated_at;
 
     public Long getId() {
         return id;
@@ -20,5 +27,37 @@ public class User {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getSub() {
+        return sub;
+    }
+
+    public void setSub(String sub) {
+        this.sub = sub;
     }
 }
